19d214fd4cac4096953286c49c5cf8f254fa69cb
Nathaniel Wilde <nathaniel.wilde@tooeleschools.org>
2020-09-10 10:17:08 -0600

    firstpush

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+class HelloWorld{
+
+public static void main(String[] args){
+		System.out.println("Hello World");
+		System.out.println("ur mum gae lol");
+		System.out.println("line 3");
+		System.out.println("line 4");
+		System.out.println("line 5");
+		System.out.println("line 6");
+		System.out.println("line 8");
+		System.out.println("line 9");
+		System.out.println("line 10");
+
+	}
+
+}
